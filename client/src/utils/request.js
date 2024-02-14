5be0cb1e0581aeebeba0808222200624d263465a
Nader Gh <nader.ghanawi78@gmail.com>
2024-02-14 03:43:16 +0200

    up

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -7,7 +7,7 @@ import axios from "axios";
 // http://localhost:3001/
 // https://mern-blog-w1a9.onrender.com
 const request = axios.create({
-    baseURL: "http://localhost:3001"
+    baseURL: "http://localhost:3001" 
 });
 
 export default request;
