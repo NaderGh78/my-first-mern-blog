4dd3722b0e5de61233c4d57d56a61967212e33b6
Nader Gh <nader.ghanawi78@gmail.com>
2024-06-26 04:08:13 +0300

    d

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -12,7 +12,7 @@ const localy = "http://localhost:3001";
 const production = "https://mern-blog-njw7.onrender.com";
 
 const request = axios.create({
-    baseURL: localy
+    baseURL: production
 });
 
 export default request;
